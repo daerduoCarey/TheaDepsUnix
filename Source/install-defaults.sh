46ca41a06683fb5f453ecb28c59842af8e92e48d
sidch <sidch@pyg>
2014-11-11 19:41:22 -0500

    Fix script path

--- Source/install-defaults.sh
+++ Source/install-defaults.sh
@@ -3,4 +3,4 @@
 SCRIPT_DIR=`dirname "$0"`
 BASE_DIR="`perl -e 'use Cwd "abs_path";print abs_path(shift)' "$SCRIPT_DIR"`"
 
-"${BASE_DIR}/install" --with-boost --with-cgal --with-lib3ds --with-cluto --with-freeimageplus --with-arpack++ --with-opt++ $@
+"${BASE_DIR}/install.sh" --with-boost --with-cgal --with-lib3ds --with-cluto --with-freeimageplus --with-arpack++ --with-opt++ $@
